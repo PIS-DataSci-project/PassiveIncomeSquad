0519b37541a785952af8b5c3d2ec33238748e351
claudiarom <claudia.romanello@studio.unibo.it>
2025-06-16 17:54:07 +0200

    Added DataModelClasses file

---
+++ DataModelClasses.py
@@ -0,0 +1,58 @@
+# CLASSES
+
+class IdentifiableEntity:
+    def __init__(self, identifier):
+        if type(identifier) == list:
+            self.id = []
+            for id in identifier:
+                self.id.append(id)
+            self.identifier = self.id
+        self.identifier = identifier 
+  
+    def getIds(self):
+        if type(self.identifier) == list:
+            return self.identifier
+        ListIDS = list()
+        ListIDS.append(self.identifier)
+        return ListIDS
+
+
+class Journal(IdentifiableEntity):
+    def __init__(self, identifier, title, language, publisher, seal, license, apc):
+        super().__init__(identifier)
+        self.title = title
+        self.publisher = publisher
+        self.language = language
+        self.seal = seal
+        self.license = license
+        self.apc = apc
+        
+    def getTitle(self):
+        return self.title
+
+    def getPublisher(self):
+        return self.publisher
+    
+    def getLanguage(self):
+        return self.language
+
+    def getDOAJSeal(self):
+        return self.seal
+    
+    def getLicense(self):
+        return self.license
+
+    def getAPC(self):
+        return self.apc
+    
+    # def getCategories(self):
+        # boh if type(self.categories) == list:
+        #     return self.categories
+        # listIDS = list()
+        # listIDS.append(self.categories)
+        # return listIDS
+    
+    # def getAreas(self):
+        # boh
+
+# aggiungere anche handler e uploadhandler ?
