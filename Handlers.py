6cc869a57553600832b8d0c74eaecdc384c3da4a
claudiarom <claudia.romanello@studio.unibo.it>
2025-10-09 18:00:46 +0200

    Update Handlers.py

--- Handlers.py
+++ Handlers.py
@@ -1,1 +1,20 @@
+import pandas as pd
+from pandas import read_csv, DataFrame
+from rdflib import Graph, URIRef, RDF, Literal
+from rdflib.plugins.stores.sparqlstore import SPARQLUpdateStore
+from Entities import *
+
+class Handler: 
+    def __init__(self, dbPathOrUrl=None):
+        self.dbPathOrUrl = dbPathOrUrl
+
+    def getdbPathOrUrl(self):
+        return self.dbPathOrUrl
+
+    def setdbPathOrUrl(self, dbPathOrUrl): # enables to set a new path or URL for the database to handle.
+        if dbPathOrUrl.strip():
+            self.dbPathOrUrl = dbPathOrUrl
+            return True # CAPIRE 
+        else:
+            return False
 
