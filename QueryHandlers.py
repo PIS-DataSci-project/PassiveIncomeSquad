0b02f164b9149e54177814f938649d1d15095cd3
polinakhrm <pkhromtsova2001@gmail.com>
2025-12-13 15:39:07 +0100

    queryhandlers

---
+++ QueryHandlers.py
@@ -0,0 +1,11 @@
+class QueryHandler(Handler):
+ """
+ Base class for executing queries against a database.
+ """
+ 
+def __init__(self, dbPathOrUrl: str):
+    super().__init__(dbPathOrUrl)
+
+def getById(self, entity_id: str) -> pd.DataFrame:
+        pass
+  
