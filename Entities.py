63704174a6863eb45c7bfd5212e404dba24c9570
claudiarom <claudia.romanello@studio.unibo.it>
2026-01-18 14:00:44 +0100

    import

--- Entities.py
+++ Entities.py
@@ -4,8 +4,6 @@ class IdentifiableEntity(object): # CLAUDIA
             self.id = []
             for id in identifier:
                 self.id.append(id)
-            self.identifier = self.id
-        else:
             self.identifier = identifier
              
     def getIds(self):
