49f68bf94c272898bc6a5fd2e7f91d2fa6bc2239
claudiarom <claudia.romanello@studio.unibo.it>
2026-01-24 11:35:15 +0100

    various

--- test.py
+++ test.py
@@ -32,10 +32,12 @@ class TestProjectBasic(unittest.TestCase):
     graph = "http://127.0.0.1:9999/blazegraph/sparql"
     
     def test_01_JournalUploadHandler(self):
+        print("\n✅ TEST 1: JournalUploadHandler")
         u = JournalUploadHandler()
         self.assertTrue(u.setDbPathOrUrl(self.graph))
         self.assertEqual(u.getDbPathOrUrl(), self.graph)
         self.assertTrue(u.pushDataToDb(self.journal))
+        print("   ✓ Upload handler working correctly")
 
     def test_02_CategoryUploadHandler(self):
         u = CategoryUploadHandler()
