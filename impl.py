a9cb1d4363d8a4450f06a58a1e4c6854e31d1e37
ChenQingHa <122341148+ChenQingHa@users.noreply.github.com>
2026-01-25 08:05:30 +0800

    fix

--- impl.py
+++ impl.py
@@ -288,7 +288,8 @@ class CategoryUploadHandler(UploadHandler):  # River
 
         # ⭐ 真正生成 DataFrame
         df = pd.DataFrame(rows)
-        return True
+        return df
+
 
 
 #---------------------------------------------------------------------------------------------
