bdbea9d41b05ee26c34fc81ed2f18bb1b148a18b
Fahmyrose <fahmyrose00@gmail.com>
2026-01-25 16:41:39 +0100

    put in folder

--- tests/River_test.py
+++ tests/River_test.py
@@ -32,7 +32,7 @@ class TestProjectBasic(unittest.TestCase):
     journal = "data" + sep + "doaj.csv"
     category = "data" + sep + "scimago.json"
     relational = "." + sep + "relational.db"
-    graph = "http://192.168.1.22:9999/blazegraph/sparql"
+    graph = "http://192.168.78.106:9999/blazegraph/"
 
     
     def test_01_JournalUploadHandler(self):
