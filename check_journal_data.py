3204878d48ba259292c7fb23444bc28a761a9673
claudiarom <claudia.romanello@studio.unibo.it>
2026-02-07 18:47:52 +0100

    organized

--- check_journal_data.py
+++
@@ -1,9 +0,0 @@
-from impl import JournalQueryHandler
-
-jh = JournalQueryHandler()
-jh.setDbPathOrUrl('./relational.db')
-df = jh.getAllJournals()
-
-print(f'Total journals: {len(df)}')
-print(f'Columns: {df.columns.tolist()}')
-print(df.head())
